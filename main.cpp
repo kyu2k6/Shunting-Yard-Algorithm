727c45eb526f4c0045a00823b575c5a366d81e9d
Kevin Yu <xiyangyang2006@gmail.com>
2023-03-17 14:26:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,121 +2,72 @@
 //3-17-23
 //Shunting Yard Algorithm creates binary tree for prefix infix postfix
 
-//MAKE SURE TO SET NON USED LINKED LIST SPOTS AS NULL OR ITS ERROR
-
 #include <iostream>
 #include <cstring>
 #include "node.h"
 
+//make a linkedlist for the operators
+//add rest to output queue
+
 using namespace std;
 
-void push(Node*& top, char* value); //add
-void pop(Node*& top); //remove
-void peek(); 
-void enqueue(Node*& front, Node*& back, char* value); //add
-void dequeue(); //remove
-int precedence(char* p);
+void enqueue(char value);
+void push(Node*& operators, char value);
+void order(Node*& output, char input);
 
-void infix();
-void prefix();
-void postfix();
+Node* output = new Node();
+Node* operators = new Node();
+Node* head = NULL;
 
 int main() {
-	
-	Node* top = NULL;
-	Node* front = NULL;
-	Node* back = NULL;
 
-	char** table = new char * [100];
-	
-	char expression[100];
-	char notation[20];
-	
-	cout << "Please input an equation: ";
-	cin.get(expression, 100);
-	cin.get();
-	
-	int index = 0;
-	//take from input and put in table
-	for (int i = 0; i < strlen(expression); i++) {
-		if (expression[i] != ' ') {
-			char* tmp = new char[1];
-			tmp[0] = expression[i];
-			table[index] = tmp;
-			index++;
-		}
-	}
-	int count = 0;
-	/*
-	while (count < index) {
-		//if (precedence(table[index]) == 0) { //If token is a number
-                    enqueue(front, back, table[index]);
+        char input[100];
+        cout << "Please input an equation: ";
+        cin.get(input, 100);
+        cin.get();
+
+        int first = 0;
+
+        for (int i = 0; i < strlen(input); i++) {
+                if (order(input[i]) == 0) {
+
+                        enqueue(output, input[i])
                 }
-                if (precedence(table[index]) == 1 || precedence(table[index]) == 2 || precedence(table[index]) == 3) { //If token is a operator
-                    if (top != NULL) {
-                        while (precedence(top->getValue()) >= precedence(table[index]) && *top->getValue() != '(') {
-                            enqueue(front, back, top->getValue()); 
-                            pop(top);
-                            if (top == NULL) {
-                                break;
-                            }
-                        }
-                    }
-                    push(top, table[index]); 
+                else {
+                        //put into operator stack
                 }
-		count++;
-	}
-	*/
+        }
+}
+
+void enqueue(Node*& output, char value) {
+        Node* old = new Node();
+        Node* tempNode;
+        Node* tempOld;
+        Node* tempHead;
+
+        if (head == NULL) {
+                tempNode = new Node(
+        }
+}
+
+void push(Node*& operators, char value) {
 
 }
 
-void push(Node*& top, char* value) {
-	Node* temp = new Node();
-	temp->setValue(value);
-	temp->setNext(top);
-	top = temp;
-}
-
-void pop(Node*& top) { //Pop function
-    if (top == NULL) {
-        cout << "Empty" << endl;
-    }
-    else {
-        Node* temp = top;
-        top = top->getNext();
-        temp->setNext(NULL);
-    }
-}
-
-void enqueue(Node*& front, Node*& back, char* value) { //Enqueue function
-    Node* temp = new Node();
-    temp->setValue(value);
-    temp->setNext(NULL);
-    if (front == NULL) { //No nodes yet
-        front = temp;
-        back = temp;
-    }
-    else { //At least one node exists
-        back->setNext(temp);
-        back = temp;
-    }
-}
-
-int precedence(char* p) {
-    if (*p == '^') {
+int order(char input) {
+    if (input == '^') {
         return 3; //Highest
     }
-    else if (*p == '*' || *p == '/') {
+    else if (input == '*' || *p == '/') {
         return 2; //Second highest
     }
-    else if (*p == '+' || *p == '-') {
+    else if (input == '+' || *p == '-') {
         return 1; //Second lowest
     }
-    else if (*p == '(' || *p == ')') {
+    else if (input == '(' || *p == ')') {
         return 4; //Can be anything, as long as it is differentiated from the rest
     }
     else {
         return 0; //Lowest, which are the numbers
     }
 }
-
